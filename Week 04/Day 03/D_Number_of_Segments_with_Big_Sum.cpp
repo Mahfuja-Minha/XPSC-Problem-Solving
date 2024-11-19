b8b7bef2448882d7f0ccfe265df7b9cd8ccc255a
Mahfuja-Minha <mahfujaakterminha@gmail.com>
2024-11-19 23:14:01 +0600

    Add files via upload

---
+++ Week 04/Day 03/D_Number_of_Segments_with_Big_Sum.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ll n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    ll l = 0, r = 0, sum = 0, cnt = 0;
+    while (r < n)
+    {
+        sum += v[r];
+        if (sum >= k)
+        {
+            cnt += (r - l + 1);
+            while (sum < k && l <= r)
+            {
+                sum -= v[l];
+                l++;
+            }
+            if (sum >= k)
+            {
+                cnt += (r - l + 1);
+            }
+        }
+        r++;
+    }
+    cout << cnt << '\n';
+    return 0;
+}
