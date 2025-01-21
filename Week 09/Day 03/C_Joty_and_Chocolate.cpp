c5495800cbb7871ff2c66004883437cb2d7d954f
Mahfuja-Minha <mahfujaakterminha@gmail.com>
2025-01-22 01:59:06 +0600

    Update C_Joty_and_Chocolate.cpp

--- Week 09/Day 03/C_Joty_and_Chocolate.cpp
+++ Week 09/Day 03/C_Joty_and_Chocolate.cpp
@@ -1,7 +1,17 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
+ll LCM(ll a, ll b)
+{
+    return (a / __gcd(a, b)) * b;
+}
 int main()
 {
+    ll n, a, b, p, q;
+    cin >> n >> a >> b >> p >> q;
 
+    ll cnt1 = (n / a) * p, cnt2 = (n / b) * q, overlap = (n / LCM(a, b));
+    ll result = ((cnt1 + cnt2) - (overlap * (p + q))) + (overlap * max(p, q));
+    cout << result << '\n';
     return 0;
 }
