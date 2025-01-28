dda46b9e01fa28374f41d7c97237c16c878587ef
Mahfuja-Minha <mahfujaakterminha@gmail.com>
2025-01-28 23:30:08 +0600

    Update A_Palindrome_Reorder.cpp

--- Week 10/Day 02/A_Palindrome_Reorder.cpp
+++ Week 10/Day 02/A_Palindrome_Reorder.cpp
@@ -2,6 +2,56 @@
 using namespace std;
 int main()
 {
+    string s;
+    cin >> s;
+    map<char, int> cnt;
+    int n = s.size();
+    for (int i = 0; i < n; i++)
+    {
+        cnt[s[i]]++;
+    }
+
+    int odd = 0;
+    for (auto [k, o] : cnt)
+    {
+        if (o % 2 != 0)
+        {
+            odd++;
+        }
+    }
+
+    if (odd > 1)
+    {
+        cout << "NO SOLUTION";
+        return 0;
+    }
+
+    string f, m, l;
+
+    for (auto [k, o] : cnt)
+    {
+        if (o % 2 != 0)
+        {
+
+            for (int i = 0; i < o; i++)
+            {
+                m.push_back(k);
+            }
+        }
+        else
+        {
+
+            for (int i = 0; i < (o / 2); i++)
+            {
+                f.push_back(k);
+                l.push_back(k);
+            }
+        }
+    }
+
+    reverse(l.begin(), l.end());
+
+    cout << f << m << l;
 
     return 0;
 }
