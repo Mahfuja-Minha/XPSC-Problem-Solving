8a7c8b8454e0d7d1803f79f52970684109e9d90d
Mahfuja-Minha <mahfujaakterminha@gmail.com>
2025-02-03 23:46:45 +0600

    Update D_Yogurt_Sale.cpp

--- Week 11/Day 02/D_Yogurt_Sale.cpp
+++ Week 11/Day 02/D_Yogurt_Sale.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 int main()
 {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, a, b;
+        cin >> n >> a >> b;
+        int x = n * a;
+        if (n == 2)
+        {
+            cout << min(x, b) << '\n';
+        }
+        else if (n % 2 == 0)
+        {
+            int c = (n / 2) * b;
+            cout << min(c, x) << '\n';
+        }
+        else
+        {
+            int y = (((n - 1) / 2) * b) + a;
+            cout << min(x, y) << '\n';
+        }
+    }
 
     return 0;
 }
