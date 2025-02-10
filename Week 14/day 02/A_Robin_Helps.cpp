21470c9d71901896696dc6deeea588ef87394dd9
Mahfuja-Minha <mahfujaakterminha@gmail.com>
2025-02-10 23:36:03 +0600

    Update A_Robin_Helps.cpp

--- Week 14/day 02/A_Robin_Helps.cpp
+++ Week 14/day 02/A_Robin_Helps.cpp
@@ -2,6 +2,36 @@
 using namespace std;
 int main()
 {
-
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        int sum = 0, cnt = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] >= k)
+            {
+                sum += a[i];
+            }
+            else if (a[i] == 0)
+            {
+                if (sum != 0)
+                {
+                    sum -= 1;
+                    cnt++;
+                }
+            }
+        }
+        cout << cnt << '\n';
+    }
     return 0;
 }
