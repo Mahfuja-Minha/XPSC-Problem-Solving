84ab835a1ce4df376fd226f544baa6545bb7fd97
Mahfuja-Minha <mahfujaakterminha@gmail.com>
2025-02-19 01:03:29 +0600

    Update C_Rudolf_and_the_Ugly_String.cpp

--- Week 15/Day 04/C_Rudolf_and_the_Ugly_String.cpp
+++ Week 15/Day 04/C_Rudolf_and_the_Ugly_String.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
     int t;
@@ -11,44 +10,18 @@ int main()
         cin >> n;
         string s;
         cin >> s;
-
         int cnt = 0;
-        while (true)
+        for (int i = 0; i < n - 2; i++)
         {
-            bool found = false;
-
-            // "pie" খোঁজা
-            for (int i = 0; i <= n - 3; i++)
+            string t = s.substr(i, 3);
+            if (t == "map" || t == "pie")
             {
-                if (s.substr(i, 3) == "pie")
-                {
-                    s.erase(i, 3);
-                    cnt++;
-                    found = true;
-                    break;
-                }
-            }
-
-            // "map" খোঁজা
-            for (int i = 0; i <= n - 3; i++)
-            {
-                if (s.substr(i, 3) == "map")
-                {
-                    s.erase(i, 3);
-                    cnt++;
-                    found = true;
-                    break;
-                }
-            }
-
-            // যদি কোনটাই না পাওয়া যায়, লুপ থামাও
-            if (!found)
-            {
-                break;
+                i += 2;
+                cnt++;
             }
         }
-
-        cout << cnt << '\n'; // মুছে দেওয়া ক্যারেক্টারের সংখ্যা
+        cout << cnt << '\n';
     }
+
     return 0;
 }
