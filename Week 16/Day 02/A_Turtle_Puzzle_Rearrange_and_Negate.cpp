38c98df776b90f8e47e7a12183e021c1e5390bd0
Mahfuja-Minha <mahfujaakterminha@gmail.com>
2025-03-02 23:56:48 +0600

    Update A_Turtle_Puzzle_Rearrange_and_Negate.cpp

--- Week 16/Day 02/A_Turtle_Puzzle_Rearrange_and_Negate.cpp
+++ Week 16/Day 02/A_Turtle_Puzzle_Rearrange_and_Negate.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 int main()
 {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, sum = 0;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+            sum += abs(a[i]);
+        }
+        cout << sum << '\n';
+    }
 
     return 0;
 }
