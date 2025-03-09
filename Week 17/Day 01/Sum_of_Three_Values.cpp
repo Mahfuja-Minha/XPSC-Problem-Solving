f84f3b5fe526390b9c0079c3e7250e9904580bf4
Mahfuja-Minha <mahfujaakterminha@gmail.com>
2025-03-10 00:57:59 +0600

    Update Sum_of_Three_Values.cpp

--- Week 17/Day 01/Sum_of_Three_Values.cpp
+++ Week 17/Day 01/Sum_of_Three_Values.cpp
@@ -1,7 +1,44 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
+
 int main()
 {
+    int n, x;
+    cin >> n >> x;
 
+    vector<pair<int, int>> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i].first;
+        a[i].second = i + 1;
+    }
+
+    sort(a.begin(), a.end());
+    for (int i = 0; i < n; i++)
+    {
+        int t = x - a[i].first;
+        int l = i + 1, r = n - 1;
+
+        while (l < r)
+        {
+            int sum = a[l].first + a[r].first;
+            if (sum == t)
+            {
+                cout << a[i].second << " " << a[l].second << " " << a[r].second << '\n';
+                return 0;
+            }
+            else if (sum < t)
+            {
+                l++;
+            }
+            else
+            {
+                r--;
+            }
+        }
+    }
+
+    cout << "IMPOSSIBLE" << '\n';
     return 0;
 }
