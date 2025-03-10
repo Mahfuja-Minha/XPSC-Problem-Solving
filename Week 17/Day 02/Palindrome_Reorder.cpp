f55b3ded185fa1a9f390944cef6fb8e8d2ff127f
Mahfuja-Minha <mahfujaakterminha@gmail.com>
2025-03-10 23:46:43 +0600

    Update Palindrome_Reorder.cpp

--- Week 17/Day 02/Palindrome_Reorder.cpp
+++ Week 17/Day 02/Palindrome_Reorder.cpp
@@ -2,6 +2,46 @@
 using namespace std;
 int main()
 {
-
+    string s;
+    cin >> s;
+    map<char, int> mp;
+    for (int i = 0; i < s.size(); i++)
+    {
+        mp[s[i]]++;
+    }
+    int odd = 0;
+    for (auto [k, o] : mp)
+    {
+        if (o % 2 != 0)
+        {
+            odd++;
+        }
+    }
+    if (odd > 1)
+    {
+        cout << "NO SOLUTION";
+        return 0;
+    }
+    string first, middle, last;
+    for (auto [k, o] : mp)
+    {
+        if (o % 2 != 0)
+        {
+            for (int i = 0; i < o; i++)
+            {
+                middle.push_back(k);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < (o / 2); i++)
+            {
+                first.push_back(k);
+                last.push_back(k);
+            }
+        }
+    }
+    reverse(last.begin(), last.end());
+    cout << first + middle + last;
     return 0;
 }
